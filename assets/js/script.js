d5308028201b77d153e64d4687ec980a8adf28d0
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2023-12-17 14:54:45 +0000

    Create functions for calculations

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+function runGame() {
+    
+}
+
+function verifyAnswer () {
+
+}
+
+function calcAnswer () {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer () {
+
+}
+
+function dispAddQuestion () {
+
+}
+
+function dispSubQuestion () {
+
+}
+
+function dispMultQuestion () {
+
+}
+function dispDivQuestion () {
+
+}
